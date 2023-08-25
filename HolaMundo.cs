155827b5d04abce502a03f6d0c39d8802e819771
sttxxp <143148459+sttxxp@users.noreply.github.com>
2023-08-24 20:53:59 -0500

    Add files via upload

---
+++ HolaMundo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hola Mundo, Este es mi juego");
+                     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
